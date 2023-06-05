c442a93eff4df32a57199a54aab358b95e4e812b
yangbiolab <yangfenglong@gmail.com>
2023-06-05 20:33:30 +0800

    This is a new commit for what I originally planned to be amended

---
+++ yangppts/assets/selectAll-378e90f0-84fbee98.js
@@ -0,0 +1,1 @@
+import{be as t,bf as n,bg as o}from"./index-57d11751.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
